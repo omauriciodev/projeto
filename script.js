5a454bb745ad6b3908d238c8695cba0932605b95
Mauriciotzk <mauriciomartinstai@gmail.com>
2026-03-06 12:46:56 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  //  if (html.classList.contains("light")) {
+  //    html.classList.remove("light")
+  //  } else {
+  //    html.classList.add("light")
+  //  }
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+  // substituir a img
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a iamgem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // se tiver sem light mode, manter a iamgem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
